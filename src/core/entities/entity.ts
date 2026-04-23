87a024c6830dd2eda877e10eeadccfcae5a42240
samueldamatta <samueldamattaid@gmail.com>
2026-04-23 20:34:02 -0300

    Implement UniqueEntityId class and refactor Entity class to use it for ID management

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto";
+import { UniqueEntityId } from "./unique-entity-id.js";
 
 export class Entity<Props> {
-    private _id: string;
+    private _id: UniqueEntityId;
     protected props: Props;
 
-    get id(): string {
-        return this._id;
+    get id() {
+        return this._id
     }
 
     constructor(props: Props, id?: string) {
-        this._id = id ?? randomUUID();
+        this._id = new UniqueEntityId(id)
         this.props = props;
     }
 }
